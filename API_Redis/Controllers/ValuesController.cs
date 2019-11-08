18d1741950c6381654730f34cfd9e5ce290ccc6d
Thiago Costa <thiagocostanascimento@gmail.com>
2019-11-07 22:43:08 -0300

    ASP.NET Core 2.2 + Redis

---
+++ API_Redis/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Configuration;
+using System;
+
+namespace API_Redis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+
+        [HttpGet]
+        public ContentResult Get(
+            [FromServices]IConfiguration config,
+            [FromServices]IDistributedCache cache)
+        {
+            string retorno = null;
+            try
+            {
+                retorno = cache.GetString("time");
+                if (retorno == null)
+                {
+                    retorno = "Que horas são? " + DateTime.Now.ToShortTimeString();
+
+                    DistributedCacheEntryOptions opcoesCache = new DistributedCacheEntryOptions();
+                    opcoesCache.SetAbsoluteExpiration(TimeSpan.FromMinutes(2));
+
+                    cache.SetString("time", retorno, opcoesCache);
+                }
+            }
+            catch (Exception ex)
+            {
+                retorno = "Ocorreu um erro: " + ex.Message + "<br>" + ex.StackTrace + "<br>" + ex.InnerException;
+            }
+            return Content(retorno);
+        }
+    }
+}
